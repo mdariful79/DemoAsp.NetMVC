29d42921cdcf87bad3712a183bcf23965f01bd2e
mdariful79 <mdariful057@gmail.com>
2026-04-18 21:12:11 +0600

    Added TestController>Model>Views

---
+++ DemoWeb/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using DemoWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWeb.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new TestHome { Name = "Ariful Islam", Email = "ariful@gmail.com" };
+            return View(model);
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Index (TestHome model)
+        {
+            var email = model.Email;
+            var name = model.Name;
+            
+            return View(model);
+        }
+    }
+}
---
+++ DemoWeb/Models/TestHome.cs
@@ -0,0 +1,8 @@
+﻿namespace DemoWeb.Models
+{
+    public class TestHome
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
