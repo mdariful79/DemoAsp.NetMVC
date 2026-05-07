b10e88a719323b9d4451f11cf86aa4de5bf3f707
mdariful79 <mdariful057@gmail.com>
2026-05-07 20:32:40 +0600

    implemented uow and slightly repository pattern

---
+++ src/DemoInfrastructure/Data/Repository.cs
@@ -0,0 +1,16 @@
+﻿using DemoDomain.Contaracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoInfrastructure.Data
+{
+    public interface Repository<TAggregateRoot, TKey> : IRepository<TAggregateRoot, TKey>
+        where TAggregateRoot : class, IAggregateRoot<TKey>
+        where TKey : IComparable
+    {
+        void Add(TAggregateRoot entity);
+        void AddAsync(TAggregateRoot entity);
+        
+    }
+}
--- src/DemoInfrastructure/Data/UnitOfWork.cs
+++ src/DemoInfrastructure/Data/UnitOfWork.cs
@@ -1,9 +1,34 @@
 ﻿using DemoDomain.Contracts;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DemoInfrastructure.Data
 {
-  
+    public abstract class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly DbContext _dbContext;
+
+        public UnitOfWork(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public async Task SaveAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+
 }
